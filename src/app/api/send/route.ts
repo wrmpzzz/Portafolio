3d636aaedba47768c0c0f2e0624bd7489f285eb8
Williams <wm0803687@gmail.com>
2025-09-01 18:49:34 -0400

    Fixed resend

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend"
 import { EmailTemplate } from '@/components/email-template';
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend("re_ARreMvSk_JiDcQpmB4mEGWitmLGvvXhFN")
 
 export async function POST(req: Request) {
     try {
